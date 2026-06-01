18db52716e6945476359e591989e25c888fb98bb
3bd-ulrahman <abdulrahman198276301@gmail.com>
2026-06-01 23:11:22 +0300

    feat: add permissions to form

--- app/Filament/Resources/Roles/Pages/CreateRole.php
+++ app/Filament/Resources/Roles/Pages/CreateRole.php
@@ -3,9 +3,33 @@
 namespace App\Filament\Resources\Roles\Pages;
 
 use App\Filament\Resources\Roles\RoleResource;
+use App\Filament\Resources\Roles\Schemas\RoleForm;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRole extends CreateRecord
 {
     protected static string $resource = RoleResource::class;
+
+    /**
+     * @var array<int, string>
+     */
+    protected array $permissionIds = [];
+
+    /**
+     * @param  array<string, mixed>  $data
+     * @return array<string, mixed>
+     */
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $this->permissionIds = RoleForm::selectedPermissionIds($data);
+
+        unset($data['permissions']);
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        $this->record->syncPermissions($this->permissionIds);
+    }
 }
